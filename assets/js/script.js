aa22d75ae8bd0b3758e5dc2561c1abeb3dc81701
tilleyarthur <86635878+tilleyarthur@users.noreply.github.com>
2021-08-14 23:41:36 -0400

    finally it does something

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,29 @@
 // Assignment code here
-// Assignment code here
 var length;
 var number;
 var specialCharacter;
 var upperCase;
-var lowåerCase;
+var lowerCase;
 var selection;
 var lettersUp
 
-const characters = ["number", "letters", "character"];
+const characters = ["number", "lettersD", "character"];
 
-number = '0123456789';
-letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-character = '!@#$%^&*)+=-([}{]:;"';
+number = "0123456789";
+lettersD = "abcdefghijklmnopqrstuvwxyz";
+lettersUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+character = "!@#$%^&*)(";
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
+  passwordText.value = password;
+}
 // Begin setting password parameters
 function generatePassword() {
-  document.getElementById("click", generatePassword); {
-    for ( let i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-  }
-
   // Select # of characters for new password
   length = prompt("Enter the number of characters you'd like for your new password (Select a number between 8-128)");
   if (!length) {
@@ -38,20 +40,20 @@ function generatePassword() {
   };
   // for all of the above true
   if (number && specialCharacter && upperCase && lowerCase) {
-    selection = character.concat(number,letters,lettersUp);
+    selection = character.concat(number,lettersD,lettersUp);
   }
   // three options selected
   else if (number && specialCharacter && upperCase) {
     selection = character.concat(number, lettersUp);
   }
   else if (specialCharacter && upperCase && lowerCase) {
-    selection = character.concat(letters, lettersUp);
+    selection = character.concat(lettersD, lettersUp);
   }
   else if (upperCase && lowerCase && number) {
-    selection = lettersUp.concat(letters, number);
+    selection = lettersUp.concat(lettersD, number);
   }
   else if (lowerCase && number && specialCharacter) {
-    selection = letters.concat(number, character);
+    selection = lettersD.concat(number, character);
   }
   //two options selected
   else if (specialCharacter && number) {
@@ -61,7 +63,7 @@ function generatePassword() {
     selection = number.concat(lettersUp);
   }
   else if (upperCase && lowerCase) {
-    selection = lettersUp.concat(letters);
+    selection = lettersUp.concat(lettersD);
   }
   else if (lowerCase && number) {
     selection = letters.concat(number);
@@ -85,58 +87,12 @@ function generatePassword() {
   else if (upperCase) {
     selection = lettersUp;
   };
+  return selection;
 
-  generatePassword.addEventListener("click", password)
+  
 }
 
-generatePassword();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
